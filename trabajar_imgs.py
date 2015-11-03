e7c08e507c9deddfc819b83be496af03471a6853
jota svec <jotasvec@gmail.com>
2015-11-03 17:46:24 -0300

    se agrega index html

---
+++ trabajar_imgs.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+#resize a image
+
+import Image
+import cv2
+import numpy as np
+import cv
+
+#se abre una imagen
+
+img_fg = cv2.imread("img/dudegreen.jpg")
+img_bg = cv2.imread("img/imagen.png")
+
+size_bg = img_bg.shape[:2] #obtenemos el ancho largo y canal? de la imagen
+size_fg = img_fg.shape[:2]
+
+cv2.imshow("imagen1", img_bg)
+cv2.imshow('imagen2', img_fg)
+
+print "tamaño frente: ",size_fg
+print "tamaño bg: ",size_bg
+
+(x,y) = size_bg
+#re escalar una imagen
+if size_fg > size_bg:
+	img_fg2 = cv2.resize(img_fg, (y,x))
+	print "tamaño nuevo fg ", img_fg2.shape[:2]
+	cv2.imshow('tamaño nuevo', img_fg2)
+
+#img_fg2 = img_fg.resize((width,height))
+
+#print "tamaño frente2: ",img_fg2.size
+#print "tamaño bg2: ",img_bg2.size
+
+#cv2.imshow("imagenes2", np.hstack([img_bg2, img_fg2]))
+
+
+esc = cv2.waitKey(0) & 0XFF
+if esc == 27:
+	cv2.destroyAllWindows()
